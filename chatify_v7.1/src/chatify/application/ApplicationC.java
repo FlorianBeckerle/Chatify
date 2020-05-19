4ebb0d455200db27072e456e93dcf62d1cf4ae4b
John Lim <johnlimhtl@gmail.com>
2020-05-19 16:39:17 +0200

    chatify 7.1
    neue features, verbesserungen

---
+++ chatify_v7.1/src/chatify/application/ApplicationC.java
@@ -0,0 +1,291 @@
+package chatify.application;
+
+/* imports */
+import chatify.models.Chatroom;
+import chatify.models.Message;
+import chatify.models.User;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.util.logging.*;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class ApplicationC implements Initializable {
+    
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private ListView<Message> messageContainer;
+    @FXML
+    private ListView<User> userContainer;
+    @FXML
+    private TextField sendMsgInput;
+    @FXML
+    private Button sendMsgButton;
+    @FXML
+    private ListView<Chatroom> chatroomsContainer; //Liste aller Server
+    @FXML
+    private VBox userSettings;
+    @FXML
+    private VBox serverSettings;
+    @FXML
+    private Label usernameLabel1;
+    
+    /* Neuen Server Anlegen */
+    @FXML
+    private TextField tfCreateServerName;
+    @FXML
+    private TextField tfCreateServerPwd;
+    @FXML
+    private TextField tfCreateServerPwdRe;
+    
+    @FXML
+    private TextField tfCreateServerUserCount;
+    
+    
+    @FXML
+    private Button closeWindowBtn;
+    @FXML
+    private Button minimizeWindowBtn;
+    @FXML
+    private AnchorPane parent;
+
+    /* sonstiges */
+    private final static String VIEWNAME = "ApplicationV.fxml";
+    private static Statement statement;
+    private static Stage stage;
+    private static String username2;
+    
+    private User current;
+    private ObservableList<User> currentUserList;
+    
+    private Chatroom currentChatroom;
+    private ObservableList<Chatroom> currentChatrooms;
+    
+    private Message currentMessage;
+    private ObservableList<Message> currentMessages;
+    
+    // stage dragable machen
+    private double x = 0;
+    private double y = 0;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        makeDragable();
+    }
+    
+    // stage dragable machen
+    private void makeDragable(){
+        parent.setOnMousePressed(((event) -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        }));
+        
+        parent.setOnMouseDragged((event) -> {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+    }
+    
+    // fxml anzeigen
+    public static void show(Stage stage, Statement statement, String username, String email) {
+        try {
+            username2 = username;
+            FXMLLoader loader = new FXMLLoader(ApplicationC.class.getResource(VIEWNAME));           // view und controller erstellen
+            Parent root = (Parent) loader.load();
+            Scene scene = new Scene(root);                                                          // scene erstellen
+            stage = null;
+  
+            if (stage == null) {                                                                    // stage verwenden oder neue erzeugen
+                stage = new Stage();
+            }
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.UNDECORATED);                                                // titel bar entfernen
+            stage.setTitle("CHATIFY! Register");
+            ApplicationC applicationC = (ApplicationC) loader.getController();                      // controller erstellen
+            applicationC.statement = statement;                                                     // datenbankzugriff merken
+            System.out.println(username);
+            applicationC.init(statement, username);                                                 //staticUsername.setText("Test"+username);
+             stage.show();                                                                          // view anzeigen
+        } catch (IOException ex) {
+            Logger.getLogger(ApplicationC.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Something wrong with " + VIEWNAME + "!");
+            ex.printStackTrace(System.err);
+            System.exit(1);
+        } catch (Exception ex) {
+            Logger.getLogger(ApplicationC.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace(System.err);
+            System.exit(2);
+        }
+    }
+
+    private void init(Statement statement, String username){
+        setCurrent(new User());
+        setCurrentChatroom(new Chatroom(statement,null));
+        setCurrentMessage(new Message(null, null, null));
+        setCurrentUserList(current.getUsers(statement, null));
+        userContainer.setItems(currentUserList);
+        
+        
+        current.getUser(statement, username);
+        usernameLabel.textProperty().bindBidirectional(current.nameProperty());
+        usernameLabel1.textProperty().bindBidirectional(current.nameProperty());
+        
+        //currentChatrooms.setAll(currentChatroom.getChatrooms(statement, username));
+        setCurrentChatrooms(currentChatroom.getChatrooms(statement, username));
+        
+        chatroomsContainer.setItems(currentChatrooms);
+        
+        chatroomsContainer.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //System.out.println(""+ chatroomsContainer.getSelectionModel().getSelectedItem().getChatroomId());
+                changeChatroom(chatroomsContainer.getSelectionModel().getSelectedItem().getChatroomId());
+            }
+        });
+        
+        changeChatroom(currentChatrooms.get(0).getChatroomId());
+        
+    }
+    
+    public void changeChatroom(String chatroomId){
+        currentChatroom= new Chatroom(statement, chatroomId);
+        setCurrentMessages(currentMessage.getMessages(statement, chatroomId));
+        messageContainer.setItems(currentMessages);
+        
+        //setCurrentUserList(current.getUsers(statement, chatroomId)); //Zeigt alle User innerhalb dieses Servers
+        //userContainer.setItems(currentUserList);
+        
+        System.out.println("Aktuelle Chatroom-ID:"+ currentChatroom.getChatroomId());
+    }
+    
+    public User getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(User current) {
+        this.current = current;
+    }
+
+    public ObservableList<User> getCurrentUserList() {
+        return currentUserList;
+    }
+
+    public void setCurrentUserList(ObservableList<User> currentUserList) {
+        this.currentUserList = currentUserList;
+    }
+    
+    public Chatroom getCurrentChatroom() {
+        return currentChatroom;
+    }
+
+    public void setCurrentChatroom(Chatroom currentChatroom) {
+        this.currentChatroom = currentChatroom;
+    }
+
+    public ObservableList<Chatroom> getCurrentChatrooms() {
+        return currentChatrooms;
+    }
+
+    public void setCurrentChatrooms(ObservableList<Chatroom> currentChatrooms) {
+        this.currentChatrooms = currentChatrooms;
+    }
+
+    public Message getCurrentMessage() {
+        return currentMessage;
+    }
+
+    public void setCurrentMessage(Message currentMessage) {
+        this.currentMessage = currentMessage;
+    }
+
+    public ObservableList<Message> getCurrentMessages() {
+        return currentMessages;
+    }
+
+    public void setCurrentMessages(ObservableList<Message> currentMessages) {
+        this.currentMessages = currentMessages;
+    }
+    
+    
+    @FXML
+    public void sendMessage(ActionEvent event){
+        System.out.println(sendMsgInput.getText()+ current.getUserId()+ currentChatroom.getChatroomId());
+        currentMessage = new Message(statement, sendMsgInput.getText(), current.getUserId(), currentChatroom.getChatroomId());
+        currentMessages.add(currentMessage);
+        messageContainer.setItems(currentMessages);
+        sendMsgInput.setText("");
+    }
+    
+    @FXML
+    public void createNewChatroom(ActionEvent event){
+        /*
+        *
+        * Hie die Eingabeüberprüfung machen :)
+        *
+        */
+        currentChatrooms.add(currentChatroom.createNewChatroom(tfCreateServerName.getText(), tfCreateServerPwd.getText(), statement));
+        serverSettings.setVisible(false);
+    }
+
+    // user settings öffnen
+    @FXML
+    private void openUserSettings(ActionEvent event) {
+        userSettings.setVisible(true);
+        serverSettings.setVisible(false);
+    }
+
+    // user settings schließen
+    @FXML
+    private void closeUserSettings(ActionEvent event) {
+        userSettings.setVisible(false);
+        serverSettings.setVisible(false);
+    }
+
+    // server settings öffnen
+    @FXML
+    private void openServerSettings(ActionEvent event) {
+        serverSettings.setVisible(true);
+        userSettings.setVisible(false);
+    }
+    
+    // server settings schließen
+    @FXML
+    private void closeServerSettings(ActionEvent event) {
+        serverSettings.setVisible(false);
+        userSettings.setVisible(false);
+    }
+
+    // fenster schließen
+    @FXML
+    private void closeWindow(ActionEvent event) {
+        Stage stage = (Stage) closeWindowBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    // fenster verkleinern
+    @FXML
+    private void minimizeWindow(ActionEvent event) {
+        Stage stage = (Stage) closeWindowBtn.getScene().getWindow();
+        minimizeWindowBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                stage.setIconified(true);
+            }
+        });
+    }
+}
---
+++ chatify_v7.1/src/chatify/models/Chatroom.java
@@ -0,0 +1,124 @@
+package chatify.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Chatroom {
+
+    private final StringProperty chatroomId = new SimpleStringProperty();
+    private final StringProperty name = new SimpleStringProperty();
+    private final ObjectProperty<LocalDate> createdAt = new SimpleObjectProperty<>();
+
+    public Chatroom(Statement statement, String chatroomid) {
+        try {
+            if (chatroomid == null) {
+                setChatroomId(null);
+                setName(null);
+            } else {
+                String sql = "select * from chatroom where chatroomId = " + chatroomid;
+                ResultSet rs = statement.executeQuery(sql);
+
+                while (rs.next()) {
+                    setChatroomId(rs.getString("chatroomid"));
+                    setName(rs.getString("name"));
+                    //setCreatedAt(rs.getDate("createdat"));
+
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Chatroom.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public Chatroom(String chatroomid, String name) {
+        setName(name);
+        setChatroomId(chatroomid);
+    }
+
+    public ObservableList<Chatroom> getChatrooms(Statement statement, String username) {
+        try {
+            String sql = "select * from chatroom"; //Einschränkung fehlt nocht
+            ResultSet rs = statement.executeQuery(sql);
+            ObservableList<Chatroom> chatrooms = FXCollections.observableArrayList();
+            while (rs.next()) {
+                chatrooms.add(new Chatroom(rs.getString("chatroomid"), rs.getString("name")));
+            }
+
+            return chatrooms;
+        } catch (SQLException ex) {
+            Logger.getLogger(Chatroom.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public Chatroom createNewChatroom(String name, String password, Statement statement){
+        try {
+            String sql = "insert into CHATROOM(CHATROOMID, CREATEDAT, NAME, password) values(next value for seq_chatroom, CURRENT_DATE, "
+                    + "'"+ name +"', "
+                    + "'"+ password +"')";
+            statement.executeUpdate(sql);
+            System.out.println("Chatroom erstellt.");
+            
+            String sqlQ = "select CHATROOMID from chatroom where name='"+name+"' and password='"+password+"'"; //Einschränkung fehlt nocht
+            ResultSet rs = statement.executeQuery(sqlQ);
+            
+            rs.next();
+            return new Chatroom(rs.getString("CHATROOMID"), name);
+        } catch (SQLException ex) {
+            Logger.getLogger(Chatroom.class.getName()).log(Level.SEVERE, null, ex);
+            return new Chatroom(statement ,null);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name.getValue();
+    }
+
+    private void setName(String value) {
+        name.set(value);
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    private void setChatroomId(String value) {
+        chatroomId.set(value);
+    }
+
+    public StringProperty chatroomIdProperty() {
+        return chatroomId;
+    }
+
+    public String getChatroomId() {
+        return chatroomId.get();
+    }
+
+    private void setCreatedAt(LocalDate value) {
+        createdAt.set(value);
+    }
+
+    public ObjectProperty<?> createdAtProperty() {
+        return createdAt;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt.get();
+    }
+}
